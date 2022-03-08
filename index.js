6b85f3b47fc9f577e04931f5cbfb8fba416c5bff
rifatjahan823 <rifatjahan823@gmail.com>
2022-03-08 21:57:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// ১. কিভাবে let, const দিয়ে ভেরিয়েবল লিখতে হয়,কখন কোনটা ইউজ করে তুমি কি জানো ?
+//let changable atar value change kora jayna
+ let change = 'orange';
+ change = 'red';
+ console.log(change)
+
+ //const changeable na ata value change kora jayna
+ const notChange = 'blue'
+ console.log(notChange)
+
+//  ২.১ কিভাবে কন্ডিশন লিখতে হয়, ছয় রকমের কন্ডিশন (>, <, ===, !==, >=. <=) কোনটা কোন জিনিসের জন্য ইউজ করবে। এছাড়াও && বা ।। দিয়ে কিভাবে একাধিক কন্ডিশন এর মধ্যে দুইটাই ফুলফিল করতে হবে আবার দুইটার যেকোন একটা ফুলফিল করতে হবে, সেটা কিভাবে করবে 
